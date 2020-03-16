dfdda21a0102945a38bb913de28f4d5bbcf24ed8
jbrucker <jebrucker@gmail.com>
2020-03-16 09:41:25 +0700

    Add new defect to Auction in auction.jar, minor wording change in README

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ## Unit Tests for Auction
 
-The Auction class contains several variants, which you can select by calling `auction.setAuctionType(int n)` with n = 1, ..., 7.  Each variant contains different defects, but one variant *might* be bug-free.  Write unit tests for auction and document what defects you find in the Auction class.
+The Auction class contains several variants, most of which contain defects.
+You can select whcih auction type (variant) to use by calling `auction.setAuctionType(int n)` with n = 1, ..., 7.  Each variant contains different defects, but at least one variant *should* be bug-free.  
+
+Write unit tests for auction and document what defects you find in the table below.
+
+See links below for more information, including how to automate testing using *parameterized tests*. 
+
+
+## Description of Defects
 
 Include the name of your tests and description of the defect.
 
@@ -10,3 +18,8 @@ Include the name of your tests and description of the defect.
 |     2       | testSomethingElse | Auction accepts a new bid lower than previous best bid. |
 
 
+## References
+
+[Lab 8 Labsheet](https://skeoop.github.io/labs/Lab8-AuctionTest.pdf)
+
+[Parameterized Unit Tests](https://skeoop.github.io/unittesting/Parameterized-Tests)
--- test/auction/AuctionTest.java
+++ test/auction/AuctionTest.java
@@ -4,31 +4,35 @@ import static org.junit.Assert.*;
 
 
 public class AuctionTest {
+	// tolerance for floating point comparisons
+	private static final double TOL = 1.0E-4;
 	// test fixture for Auction
 	private Auction auction;
 
+	/** Create a "test fixture" that can be used in many tests. */
 	@Before
 	public void setUp() {
 		// create a test fixture
 		auction = new Auction("Java in Action");
 		// set the auction variant to test
 		auction.setAuctionType(3);
-		// This is for testing. Remove this.
+		// This is for testing. Remove it after verify working.
 		System.out.println("Auction type is " + auction.getAuctionType());
 	}
 
 	/** Test that a new auction has no bids and bidding is disabled. */
 	@Test
 	public void testNewAuctionState() {
-		assertEquals(0, auction.bestBid(), 0.0);
-		assertTrue( auction.isEnabled() ); // fix this
+		assertEquals(0, auction.bestBid(), TOL);
+		assertFalse( auction.isEnabled() );
 	}
 
+	/** Test that we can place a bid after auction is started. */
 	@Test
 	public void testCanBidInAuction() {
 		auction.start();
 		assertTrue("Auction should be enabled after start()", auction.isEnabled());
-		double amount = 125.0;
+		double amount = 111.0;
 		// Who is James Gosling?
 		auction.bid("James Gosling", amount);
 		assertEquals(amount, auction.bestBid(), 0.0); // fix this
